6b5dc2e1851eb002e044dc1f4ee0bee4bf92f6d3
c9u11 <tjrdud6412@naver.com>
2024-11-02 17:49:28 +0900

    ✨ week-09

---
+++ web-09/public/javascripts/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7xvDG79QetYQJjRBEqKIj7O_---1a-CA",
+  authDomain: "inha-48be0.firebaseapp.com",
+  databaseURL: "https://inha-48be0-default-rtdb.firebaseio.com",
+  projectId: "inha-48be0",
+  storageBucket: "inha-48be0.appspot.com",
+  messagingSenderId: "535435308843",
+  appId: "1:535435308843:web:9caf8947add3cd38421f6e",
+  measurementId: "G-JX62ZCZV81"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
